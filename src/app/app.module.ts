ef31d93ded74d5c5389a65f219f925492ab27016
Gerald Nnebe <geraldnebs@gmail.com>
2020-02-25 14:27:25 +0100

    update front-end components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
+/* Import the Angular modules we need */
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+/* Import all the components that make up our app */
 import { AppComponent } from './app.component';
+import { ArtworksComponent } from './artworks/artworks/artworks.component';
+import { LoginComponent } from './admin/login/login.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
 import { LandingComponent } from './landing/landing.component';
 import { FooterComponent } from './footer/footer.component';
 import { BlogLandingComponent } from './blog/blog-landing/blog-landing.component';
+import { ContactComponent } from './contact/contact.component';
+import { ViewArtComponent } from './artworks/view-art/view-art.component';
 
+/* Configure routes */
 const appRoutes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'blog', component: BlogLandingComponent },
-  { path: '', component: LandingComponent },
-  { path: '**', component: AppComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'artworks', component: ArtworksComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: '', component: LandingComponent }, // The LandingComponent is the default component
+  { path: '**', component: LandingComponent }
 ];
 
 @NgModule({
@@ -24,7 +35,11 @@ const appRoutes: Routes = [
     HeaderComponent,
     LandingComponent,
     FooterComponent,
-    BlogLandingComponent
+    BlogLandingComponent,
+    ContactComponent,
+    LoginComponent,
+    ArtworksComponent,
+    ViewArtComponent
   ],
   imports: [
     BrowserModule,
