66f9717c94c88dd0947470e5fad3dcd5b1653cc4
aptTiger <geraldnebs@gmail.com>
2020-03-27 17:13:42 +0100

    fixed some bugs

--- src/app/works/works/works.component.ts
+++ src/app/works/works/works.component.ts
@@ -21,11 +21,17 @@ export class WorksComponent implements OnInit {
     // We are going to receive at most 12 items here (based on the settings of the backend)
     // So, we should split them into groups of three's, so we can show a carousel with groups of six, three in a row
     // I should use a multidimensional array of one group, but I'm tired. Will refactor later and reduce the repetition in works.comp.html
-    // TODO if we received less than 12 items, fill up the array
+
+    // If we received less than 12 items, fill up the array
+    if (data.length < 12)
+      for (let x = data.length; x < 12; x++)
+        data[x] = null;
+
     this.groupOne = data.slice(0, 3);
-    this.groupTwo = data.slice(3,6);
-    this.groupThree = data.slice(6,9);
+    this.groupTwo = data.slice(3, 6);
+    this.groupThree = data.slice(6, 9);
     this.groupFour = data.slice(9);
+    console.log(data);
     data = [];
   }
 
