1df381a9171349e2a848c6ca549da3750d586568
aptTiger <geraldnebs@gmail.com>
2020-04-22 16:59:01 +0100

    set creds

--- src/services/api-endpoints.ts
+++ src/services/api-endpoints.ts
@@ -1,19 +1,19 @@
-export enum ApiEndpoints {
-    LOGIN = "http://localhost:3000/api/login",
-    REGISTER = "http://localhost:3000/api/register",
-
-    UPLOAD = "http://localhost:3000/api/upload",
-
-    WORK = "http://localhost:3000/api/work",
-    BLOG = "http://localhost:3000/api/blog"
-}
-
 // export enum ApiEndpoints {
-//     LOGIN = "https://geraldnnebe-chilbeth-backend.glitch.me/api/login",
-//     REGISTER = "https://geraldnnebe-chilbeth-backend.glitch.me/api/register",
+//     LOGIN = "http://localhost:3000/api/login",
+//     REGISTER = "http://localhost:3000/api/register",
 
-//     UPLOAD = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload",
+//     UPLOAD = "http://localhost:3000/api/upload",
 
-//     WORK = "https://geraldnnebe-chilbeth-backend.glitch.me/api/work",
-//     BLOG = "https://geraldnnebe-chilbeth-backend.glitch.me/api/blog"
+//     WORK = "http://localhost:3000/api/work",
+//     BLOG = "http://localhost:3000/api/blog"
 // }
+
+export enum ApiEndpoints {
+    LOGIN = "https://geraldnnebe-chilbeth-backend.glitch.me/api/login",
+    REGISTER = "https://geraldnnebe-chilbeth-backend.glitch.me/api/register",
+
+    UPLOAD = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload",
+
+    WORK = "https://geraldnnebe-chilbeth-backend.glitch.me/api/work",
+    BLOG = "https://geraldnnebe-chilbeth-backend.glitch.me/api/blog"
+}
