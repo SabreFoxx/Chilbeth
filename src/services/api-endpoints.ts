27350e14d427f1b2c5a059763cacace750637529
aptTiger <geraldnebs@gmail.com>
2020-05-02 17:54:11 +0100

    About to stop using work-special-pagination.component.ts

--- src/services/api-endpoints.ts
+++ src/services/api-endpoints.ts
@@ -1,21 +1,23 @@
-// export enum ApiEndpoints {
-//     LOGIN = "http://localhost:3000/api/login",
-//     REGISTER = "http://localhost:3000/api/register",
-
-//     UPLOAD = "http://localhost:3000/api/upload",
-//     UPLOADED_FILES = "http://localhost:3000/images/uploads",
-
-//     WORK = "http://localhost:3000/api/work",
-//     BLOG = "http://localhost:3000/api/blog"
-// }
-
+/* For local development */
 export enum ApiEndpoints {
-    LOGIN = "https://geraldnnebe-chilbeth-backend.glitch.me/api/login",
-    REGISTER = "https://geraldnnebe-chilbeth-backend.glitch.me/api/register",
+    LOGIN = "http://localhost:3000/api/login",
+    REGISTER = "http://localhost:3000/api/register",
 
-    UPLOAD = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload",
-    UPLOADED_FILES = "https://geraldnnebe-chilbeth-backend.glitch.me/images/uploads",
+    UPLOAD = "http://localhost:3000/api/upload",
+    UPLOADED_FILES = "http://localhost:3000/images/uploads",
 
-    WORK = "https://geraldnnebe-chilbeth-backend.glitch.me/api/work",
-    BLOG = "https://geraldnnebe-chilbeth-backend.glitch.me/api/blog"
+    WORK = "http://localhost:3000/api/work",
+    BLOG = "http://localhost:3000/api/blog"
 }
+
+/* For live deploy */
+// export enum ApiEndpoints {
+//     LOGIN = "https://geraldnnebe-chilbeth-backend.glitch.me/api/login",
+//     REGISTER = "https://geraldnnebe-chilbeth-backend.glitch.me/api/register",
+
+//     UPLOAD = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload",
+//     UPLOADED_FILES = "https://geraldnnebe-chilbeth-backend.glitch.me/images/uploads",
+
+//     WORK = "https://geraldnnebe-chilbeth-backend.glitch.me/api/work",
+//     BLOG = "https://geraldnnebe-chilbeth-backend.glitch.me/api/blog"
+// }
