0b7631639578e39490b5d5f0a1e6835ba026dd77
SabreFoxx <geraldnebs@gmail.com>
2021-12-21 23:45:06 +0100

    update endpoints to match backend

--- src/services/api-endpoints.ts
+++ src/services/api-endpoints.ts
@@ -1,43 +1,43 @@
 /* For local development */
 export enum ApiEndpoints {
-    LOGIN = "http://localhost:3000/api/login",
-    REGISTER = "http://localhost:3000/api/register",
-    SITE_SETTINGS = "http://localhost:3000/api/settings",
-    CHANGE_PASSWORD = "http://localhost:3000/api/change_password",
+    LOGIN = "http://localhost:3000/login",
+    REGISTER = "http://localhost:3000/register",
+    SITE_SETTINGS = "http://localhost:3000/settings",
+    CHANGE_PASSWORD = "http://localhost:3000/change_password",
 
-    UPLOAD = "http://localhost:3000/api/upload",
-    UPLOAD_LANDING = "http://localhost:3000/api/upload_landing_image",
-    UPLOAD_ARTWORK = "http://localhost:3000/api/upload_artwork",
-    UPLOAD_SITE_LOGO = "http://localhost:3000/api/upload_site_logo",
-    UPLOAD_PROFILE = "http://localhost:3000/api/upload_profile",
-    UPLOADED_FILES = "http://localhost:3000/api/images",
-    CV = "http://localhost:3000/api/cv",
+    UPLOAD = "http://localhost:3000/upload",
+    UPLOAD_LANDING = "http://localhost:3000/upload_landing_image",
+    UPLOAD_ARTWORK = "http://localhost:3000/upload_artwork",
+    UPLOAD_SITE_LOGO = "http://localhost:3000/upload_site_logo",
+    UPLOAD_PROFILE = "http://localhost:3000/upload_profile",
+    UPLOADED_FILES = "http://localhost:3000/images",
+    CV = "http://localhost:3000/cv",
 
-    WORK = "http://localhost:3000/api/work",
-    WORK_CATEGORIES = "http://localhost:3000/api/work_categories",
-    EXHIBITION = "http://localhost:3000/api/exhibition",
-    BLOG = "http://localhost:3000/api/blog",
-    NEWSLETTER = "http://localhost:3000/api/newsletter"
+    WORK = "http://localhost:3000/work",
+    WORK_CATEGORIES = "http://localhost:3000/work_categories",
+    EXHIBITION = "http://localhost:3000/exhibition",
+    BLOG = "http://localhost:3000/blog",
+    NEWSLETTER = "http://localhost:3000/newsletter"
 }
 
 /* For live deploy */
 // export enum ApiEndpoints {
-//     LOGIN = "https://geraldnnebe-chilbeth-backend.glitch.me/api/login",
-//     REGISTER = "https://geraldnnebe-chilbeth-backend.glitch.me/api/register",
-//     SITE_SETTINGS = "https://geraldnnebe-chilbeth-backend.glitch.me/api/settings",
-//     CHANGE_PASSWORD = "https://geraldnnebe-chilbeth-backend.glitch.me/api/change_password",
+//     LOGIN = "https://api.chinyereodinukwe.com/login",
+//     REGISTER = "https://api.chinyereodinukwe.com/register",
+//     SITE_SETTINGS = "https://api.chinyereodinukwe.com/settings",
+//     CHANGE_PASSWORD = "https://api.chinyereodinukwe.com/change_password",
 
-//     UPLOAD = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload",
-//     UPLOAD_LANDING = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload_landing_image",
-//     UPLOAD_ARTWORK = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload_artwork",
-//     UPLOAD_SITE_LOGO = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload_site_logo",
-//     UPLOAD_PROFILE = "https://geraldnnebe-chilbeth-backend.glitch.me/api/upload_profile",
-//     UPLOADED_FILES = "https://geraldnnebe-chilbeth-backend.glitch.me/api/images",
-//     CV = "https://geraldnnebe-chilbeth-backend.glitch.me/api/cv",
+//     UPLOAD = "https://api.chinyereodinukwe.com/upload",
+//     UPLOAD_LANDING = "https://api.chinyereodinukwe.com/upload_landing_image",
+//     UPLOAD_ARTWORK = "https://api.chinyereodinukwe.com/upload_artwork",
+//     UPLOAD_SITE_LOGO = "https://api.chinyereodinukwe.com/upload_site_logo",
+//     UPLOAD_PROFILE = "https://api.chinyereodinukwe.com/upload_profile",
+//     UPLOADED_FILES = "https://api.chinyereodinukwe.com/images",
+//     CV = "https://api.chinyereodinukwe.com/cv",
 
-//     WORK = "https://geraldnnebe-chilbeth-backend.glitch.me/api/work",
-//     WORK_CATEGORIES = "https://geraldnnebe-chilbeth-backend.glitch.me/api/work_categories",
-//     EXHIBITION = "https://geraldnnebe-chilbeth-backend.glitch.me/api/exhibition",
-//     BLOG = "https://geraldnnebe-chilbeth-backend.glitch.me/api/blog",
-//     NEWSLETTER = "https://geraldnnebe-chilbeth-backend.glitch.me/api/newsletter"
+//     WORK = "https://api.chinyereodinukwe.com/work",
+//     WORK_CATEGORIES = "https://api.chinyereodinukwe.com/work_categories",
+//     EXHIBITION = "https://api.chinyereodinukwe.com/exhibition",
+//     BLOG = "https://api.chinyereodinukwe.com/blog",
+//     NEWSLETTER = "https://api.chinyereodinukwe.com/newsletter"
 // }
