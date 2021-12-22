220fd599784674d5949f51296eeb8750a96c8242
SabreFoxx <geraldnebs@gmail.com>
2021-12-22 13:05:11 +0100

    update api endpoints

--- src/services/api-endpoints.ts
+++ src/services/api-endpoints.ts
@@ -1,43 +1,43 @@
 /* For local development */
-export enum ApiEndpoints {
-    LOGIN = "http://localhost:3000/login",
-    REGISTER = "http://localhost:3000/register",
-    SITE_SETTINGS = "http://localhost:3000/settings",
-    CHANGE_PASSWORD = "http://localhost:3000/change_password",
+// export enum ApiEndpoints {
+//     LOGIN = "http://localhost:3000/login",
+//     REGISTER = "http://localhost:3000/register",
+//     SITE_SETTINGS = "http://localhost:3000/settings",
+//     CHANGE_PASSWORD = "http://localhost:3000/change_password",
 
-    UPLOAD = "http://localhost:3000/upload",
-    UPLOAD_LANDING = "http://localhost:3000/upload_landing_image",
-    UPLOAD_ARTWORK = "http://localhost:3000/upload_artwork",
-    UPLOAD_SITE_LOGO = "http://localhost:3000/upload_site_logo",
-    UPLOAD_PROFILE = "http://localhost:3000/upload_profile",
-    UPLOADED_FILES = "http://localhost:3000/images",
-    CV = "http://localhost:3000/cv",
+//     UPLOAD = "http://localhost:3000/upload",
+//     UPLOAD_LANDING = "http://localhost:3000/upload_landing_image",
+//     UPLOAD_ARTWORK = "http://localhost:3000/upload_artwork",
+//     UPLOAD_SITE_LOGO = "http://localhost:3000/upload_site_logo",
+//     UPLOAD_PROFILE = "http://localhost:3000/upload_profile",
+//     UPLOADED_FILES = "http://localhost:3000/images",
+//     CV = "http://localhost:3000/cv",
 
-    WORK = "http://localhost:3000/work",
-    WORK_CATEGORIES = "http://localhost:3000/work_categories",
-    EXHIBITION = "http://localhost:3000/exhibition",
-    BLOG = "http://localhost:3000/blog",
-    NEWSLETTER = "http://localhost:3000/newsletter"
-}
+//     WORK = "http://localhost:3000/work",
+//     WORK_CATEGORIES = "http://localhost:3000/work_categories",
+//     EXHIBITION = "http://localhost:3000/exhibition",
+//     BLOG = "http://localhost:3000/blog",
+//     NEWSLETTER = "http://localhost:3000/newsletter"
+// }
 
 /* For live deploy */
-// export enum ApiEndpoints {
-//     LOGIN = "https://api.chinyereodinukwe.com/login",
-//     REGISTER = "https://api.chinyereodinukwe.com/register",
-//     SITE_SETTINGS = "https://api.chinyereodinukwe.com/settings",
-//     CHANGE_PASSWORD = "https://api.chinyereodinukwe.com/change_password",
+export enum ApiEndpoints {
+    LOGIN = "https://api.chinyereodinukwe.com:3002/login",
+    REGISTER = "https://api.chinyereodinukwe.com:3002/register",
+    SITE_SETTINGS = "https://api.chinyereodinukwe.com:3002/settings",
+    CHANGE_PASSWORD = "https://api.chinyereodinukwe.com:3002/change_password",
 
-//     UPLOAD = "https://api.chinyereodinukwe.com/upload",
-//     UPLOAD_LANDING = "https://api.chinyereodinukwe.com/upload_landing_image",
-//     UPLOAD_ARTWORK = "https://api.chinyereodinukwe.com/upload_artwork",
-//     UPLOAD_SITE_LOGO = "https://api.chinyereodinukwe.com/upload_site_logo",
-//     UPLOAD_PROFILE = "https://api.chinyereodinukwe.com/upload_profile",
-//     UPLOADED_FILES = "https://api.chinyereodinukwe.com/images",
-//     CV = "https://api.chinyereodinukwe.com/cv",
+    UPLOAD = "https://api.chinyereodinukwe.com:3002/upload",
+    UPLOAD_LANDING = "https://api.chinyereodinukwe.com:3002/upload_landing_image",
+    UPLOAD_ARTWORK = "https://api.chinyereodinukwe.com:3002/upload_artwork",
+    UPLOAD_SITE_LOGO = "https://api.chinyereodinukwe.com:3002/upload_site_logo",
+    UPLOAD_PROFILE = "https://api.chinyereodinukwe.com:3002/upload_profile",
+    UPLOADED_FILES = "https://api.chinyereodinukwe.com:3002/images",
+    CV = "https://api.chinyereodinukwe.com:3002/cv",
 
-//     WORK = "https://api.chinyereodinukwe.com/work",
-//     WORK_CATEGORIES = "https://api.chinyereodinukwe.com/work_categories",
-//     EXHIBITION = "https://api.chinyereodinukwe.com/exhibition",
-//     BLOG = "https://api.chinyereodinukwe.com/blog",
-//     NEWSLETTER = "https://api.chinyereodinukwe.com/newsletter"
-// }
+    WORK = "https://api.chinyereodinukwe.com:3002/work",
+    WORK_CATEGORIES = "https://api.chinyereodinukwe.com:3002/work_categories",
+    EXHIBITION = "https://api.chinyereodinukwe.com:3002/exhibition",
+    BLOG = "https://api.chinyereodinukwe.com:3002/blog",
+    NEWSLETTER = "https://api.chinyereodinukwe.com:3002/newsletter"
+}
